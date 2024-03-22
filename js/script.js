a721c7a5e9cb70101dc8750b321b0ebafed261f8
DaevChristian <202210819@gordoncollege.edu.ph>
2024-03-22 20:50:31 +0800

    first

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.getElementById("sidebarBtn").addEventListener("click", function() {
+    document.getElementById("mySidebar").style.width = "250px";
+});
+
+document.getElementById("closeBtn").addEventListener("click", function() {
+    document.getElementById("mySidebar").style.width = "0";
+});
+
+window.addEventListener("resize", function() {
+    if (window.innerWidth >= 1199) {
+        document.getElementById("mySidebar").style.width = "0";
+    }
+});
+
