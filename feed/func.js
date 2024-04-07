b2729188b114b866f966604fcbf96f27b7dc400a
CDave2 <202210819@gordoncollege.edu.ph>
2024-04-08 01:00:12 +0800

    idk up

---
+++ feed/func.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("myImage");
+    var modalImg = document.getElementById("img01");
+
+    img.onclick = function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+
+    var span = document.getElementsByClassName("close")[0];
+
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+}
